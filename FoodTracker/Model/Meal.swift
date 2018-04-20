5a7661c18b2f99db3064688371ab434b54e83858
Jon Holmberg <jon.holmberg@visma.com>
2018-04-20 15:11:10 +0200

    Added a model with tests

---
+++ FoodTracker/Model/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jon Holmberg on 20/04/2018.
+//  Copyright © 2018 Jon Holmberg. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
